3b31b0f5bd86f7a22f68a506fc23f5dd4d900862
Gordon Lin <gordonlin89@gmail.com>
2014-10-19 16:33:03 -0700

    Added multiple runs.

--- cpu3.c
+++ cpu3.c
@@ -1,11 +1,35 @@
 #include <unistd.h>
 #include <stdio.h>
+#include "rdtsc.h"
 
-int main() {
-	pid_t pid = fork();
-	if (pid == 0) {
-		printf("Child Process\n");
+int main(int argc, char* argv[]) {
+	int runs;
+	if (argc == 2) {
+		runs = atoi(argv[1]);
 	} else {
-		printf("Parent Process\n");
+		printf("Expecting run count\n");
 	}
+
+	unsigned long long start, end;
+
+	int i;
+	unsigned long long clock_total = 0;
+	for (i = 0; i < runs; i++) {
+		start = rdtsc();
+		pid_t pid = fork();
+		end = rdtsc();
+		unsigned long long diff = end - start;
+
+		if (pid == 0) {
+			//printf("Child Process\n");
+			return end-start;
+		} else {
+			//printf("Parent Process\n");
+			printf("%llu\n", diff);
+			clock_total = clock_total + diff;
+		}
+	}
+
+	printf("CLOCK_TOTAL: %llu\n", clock_total);
+	printf("AVG: %llu\n", clock_total/runs);
 }
