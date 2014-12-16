725e87178dc5d82bea2c87fc96bd73276fb3ae76
Gordon Lin <gordonlin89@gmail.com>
2014-12-16 09:38:09 -0800

    file cache changes

--- file_cache.sh
+++ file_cache.sh
@@ -8,6 +8,13 @@ echo -n 64,; ./file_cache.out /tmp/foobar64M $RUNS | python ./avg.py
 echo -n 128,; ./file_cache.out /tmp/foobar128M $RUNS | python ./avg.py
 echo -n 256,; ./file_cache.out /tmp/foobar256M $RUNS | python ./avg.py
 echo -n 512,; ./file_cache.out /tmp/foobar512M $RUNS | python ./avg.py
+
+echo -n 700,; ./file_cache.out /tmp/foobar700M $RUNS | python ./avg.py
+echo -n 800,; ./file_cache.out /tmp/foobar800M $RUNS | python ./avg.py
+echo -n 850,; ./file_cache.out /tmp/foobar850M $RUNS | python ./avg.py
+echo -n 900,; ./file_cache.out /tmp/foobar900M $RUNS | python ./avg.py
+echo -n 950,; ./file_cache.out /tmp/foobar950M $RUNS | python ./avg.py
+
 echo -n 1024,; ./file_cache.out /tmp/foobar1024M $RUNS | python ./avg.py
 
 echo -n 1100,; ./file_cache.out /tmp/foobar1100M $RUNS | python ./avg.py
--- makeFiles.sh
+++ makeFiles.sh
@@ -9,11 +9,11 @@ dd if=/dev/urandom of=$1/foobar128M oflag=direct bs=1M count=128
 dd if=/dev/urandom of=$1/foobar256M oflag=direct bs=1M count=256
 dd if=/dev/urandom of=$1/foobar512M oflag=direct bs=1M count=512
 
-dd if=/dev/urandom of=$1/foobar512M oflag=direct bs=1M count=700
-dd if=/dev/urandom of=$1/foobar512M oflag=direct bs=1M count=800
-dd if=/dev/urandom of=$1/foobar512M oflag=direct bs=1M count=850
-dd if=/dev/urandom of=$1/foobar512M oflag=direct bs=1M count=900
-dd if=/dev/urandom of=$1/foobar512M oflag=direct bs=1M count=950
+dd if=/dev/urandom of=$1/foobar700M oflag=direct bs=1M count=700
+dd if=/dev/urandom of=$1/foobar800M oflag=direct bs=1M count=800
+dd if=/dev/urandom of=$1/foobar850M oflag=direct bs=1M count=850
+dd if=/dev/urandom of=$1/foobar900M oflag=direct bs=1M count=900
+dd if=/dev/urandom of=$1/foobar950M oflag=direct bs=1M count=950
 
 dd if=/dev/urandom of=$1/foobar1024M oflag=direct bs=1M count=1024
 
